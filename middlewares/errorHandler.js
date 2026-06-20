d8ab388ed131f6e361b3153c04b38cb8c75b0020
“Nilde <nilde.benedetti@gmail.com>
2026-06-20 10:23:46 +0200

    aggiunta funzione per middleware errorHandler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+function errorHandler(error, request, response, next) {
+    console.log(error)
+
+    response.status(500)
+        .json({
+            error: error.message,
+            results: null
+        })
+
+
+};
+
+export default errorHandler;
