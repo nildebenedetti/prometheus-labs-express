8948c4af946818e4e9c8acc3e819279355ae5924
“Nilde <nilde.benedetti@gmail.com>
2026-06-21 12:12:44 +0200

    aggiunta funzione per rotta show a products controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 import connection from "../data/db.js";
 import { validateSlug } from "../utils_js/validation/validateSlug.js";
-import { querySelectAllProducts } from "../utils_js/queries.js";
+import queries from "../utils_js/queries/queries.js";
 
 async function index(request, response) {
     // per ora la query è una stringa di placeholder, da fixare quando abbiamo real db connection
@@ -24,13 +24,44 @@ async function index(request, response) {
         return response.status(500)
             .json({
                 results: null,
-                error: 'errore interno del server'
+                error: 'errore interno del server nel recuperare i prodotti'
             });
     }
 }
 
+async function show(request, response) {
+    const { slug: productSlug } = request.params; // valutare con Simo se spostare in un middleware validateSlug
+    const realSlug = validateSlug(productSlug);
+    console.log('lo slug del prodotto:', realSlug); // riga per check funzionamento da console
+
+    try {
+        const [ rows ] = await connection.execute(querySelectProductBySlug, [realSlug]);
+
+        if (!rows || rows.length === 0) {
+            return response.status(404)
+                    .json({
+                        results: null,
+                        error: `Il database non presenta alcun prodotto con slug ${realSlug}!`
+                    });
+        }
+
+        return response.status(200)
+                .json({
+                    results: rows,
+                    error: null
+                });
+
+    } catch (error) {
+        return response.status(500)
+                .json({
+                    results: null,
+                    error: `errore interno del server nel recuperare il prodotto con slug ${realSlug}`
+                });
+    }
+}
+
 const productsController = {
-    index
+    index, show
 };
 
 export default productsController;
