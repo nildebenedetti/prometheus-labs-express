0922ca14d47575b91e240463d38ed6c78ca74f58
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-06-22 12:46:13 +0200

    feat: update queries and refactor orders controller

--- utils_js/queries/queries.js
+++ utils_js/queries/queries.js
@@ -1,19 +1,58 @@
-/* inserire tutte le query per avere più ordine nel codice! 
-usiamo la sintassi */
-
 /*======== PRODUCTS ========*/
 
 const querySelectAllProducts = `
-inserisci qui la query
+    SELECT id, name, slug, price_full, price_discount, image_main_url
+    FROM products
+    ORDER BY created_at DESC
 `;
 
 const querySelectProductBySlug = `
-inserisci qui la query
+    SELECT *
+    FROM products
+    WHERE slug = ?
+    LIMIT 1
 `;
 
+/*======== ORDERS ========*/
+
+const queryGetAllOrders = `
+    SELECT id, guest_email, total_amount, created_at,
+    guest_name, guest_surname, city, country
+    FROM orders
+    ORDER BY created_at DESC
+`;
+
+const queryGetOrderById = `
+    SELECT *
+    FROM orders
+    WHERE id = ?
+    LIMIT 1
+`;
+
+const queryGetOrderItems = `
+    SELECT 
+        op.product_id, 
+        op.quantity, 
+        op.price_at_purchase,
+        p.name, 
+        p.slug,
+        p.image_main_url
+    FROM order_products op
+    JOIN products p ON p.id = op.product_id
+    WHERE op.order_id = ?
+`;
+
+/*======== EXPORT ========*/
+
 const queries = {
+    // Products
     querySelectAllProducts,
-    querySelectProductBySlug
+    querySelectProductBySlug,
+
+    // Orders
+    queryGetAllOrders,
+    queryGetOrderById,
+    queryGetOrderItems
 };
 
 export default queries;
